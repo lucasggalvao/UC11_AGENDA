48d1d9db0809a7abb8073f3cae34cd37b871a6f3
lucasggalvao <lucas11galvao@gmail.com>
2023-05-05 21:40:47 -0300

    Adicionar arquivos de projeto.

---
+++ UC11_AGENDA/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace UC11_AGENDA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNOME = new System.Windows.Forms.Label();
+            this.textBoxNOME = new System.Windows.Forms.TextBox();
+            this.radioButtonBLOQ = new System.Windows.Forms.RadioButton();
+            this.labelSOBRENOME = new System.Windows.Forms.Label();
+            this.textBoxSOBRENOME = new System.Windows.Forms.TextBox();
+            this.textBoxCELULAR = new System.Windows.Forms.TextBox();
+            this.labelCELULAR = new System.Windows.Forms.Label();
+            this.labelTELEFONE = new System.Windows.Forms.Label();
+            this.textBoxTELEFONE = new System.Windows.Forms.TextBox();
+            this.buttonCADASTRAR = new System.Windows.Forms.Button();
+            this.buttonALTERAR = new System.Windows.Forms.Button();
+            this.buttonEXCLUIR = new System.Windows.Forms.Button();
+            this.groupBoxCONTATO = new System.Windows.Forms.GroupBox();
+            this.radioButtonDESBLOQ = new System.Windows.Forms.RadioButton();
+            this.dataGridViewCONTATO = new System.Windows.Forms.DataGridView();
+            this.buttonPESQUISAR = new System.Windows.Forms.Button();
+            this.labelEMAIL = new System.Windows.Forms.Label();
+            this.textBoxEMAIL = new System.Windows.Forms.TextBox();
+            this.labelLINKEDIN = new System.Windows.Forms.Label();
+            this.textBoxLINKEDIN = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.groupBoxCONTATO.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCONTATO)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNOME
+            // 
+            this.labelNOME.AutoSize = true;
+            this.labelNOME.BackColor = System.Drawing.Color.Transparent;
+            this.labelNOME.ForeColor = System.Drawing.Color.White;
+            this.labelNOME.Location = new System.Drawing.Point(116, 9);
+            this.labelNOME.Name = "labelNOME";
+            this.labelNOME.Size = new System.Drawing.Size(35, 13);
+            this.labelNOME.TabIndex = 0;
+            this.labelNOME.Text = "Nome";
+            // 
+            // textBoxNOME
+            // 
+            this.textBoxNOME.Location = new System.Drawing.Point(115, 25);
+            this.textBoxNOME.Name = "textBoxNOME";
+            this.textBoxNOME.Size = new System.Drawing.Size(131, 20);
+            this.textBoxNOME.TabIndex = 1;
+            // 
+            // radioButtonBLOQ
+            // 
+            this.radioButtonBLOQ.AutoSize = true;
+            this.radioButtonBLOQ.ForeColor = System.Drawing.Color.White;
+            this.radioButtonBLOQ.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonBLOQ.Name = "radioButtonBLOQ";
+            this.radioButtonBLOQ.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonBLOQ.TabIndex = 2;
+            this.radioButtonBLOQ.TabStop = true;
+            this.radioButtonBLOQ.Text = "Bloqueado";
+            this.radioButtonBLOQ.UseVisualStyleBackColor = true;
+            // 
+            // labelSOBRENOME
+            // 
+            this.labelSOBRENOME.AutoSize = true;
+            this.labelSOBRENOME.BackColor = System.Drawing.Color.Transparent;
+            this.labelSOBRENOME.ForeColor = System.Drawing.Color.White;
+            this.labelSOBRENOME.Location = new System.Drawing.Point(9, 44);
+            this.labelSOBRENOME.Name = "labelSOBRENOME";
+            this.labelSOBRENOME.Size = new System.Drawing.Size(61, 13);
+            this.labelSOBRENOME.TabIndex = 3;
+            this.labelSOBRENOME.Text = "Sobrenome";
+            // 
+            // textBoxSOBRENOME
+            // 
+            this.textBoxSOBRENOME.Location = new System.Drawing.Point(12, 60);
+            this.textBoxSOBRENOME.Name = "textBoxSOBRENOME";
+            this.textBoxSOBRENOME.Size = new System.Drawing.Size(131, 20);
+            this.textBoxSOBRENOME.TabIndex = 4;
+            // 
+            // textBoxCELULAR
+            // 
+            this.textBoxCELULAR.Location = new System.Drawing.Point(115, 99);
+            this.textBoxCELULAR.Name = "textBoxCELULAR";
+            this.textBoxCELULAR.Size = new System.Drawing.Size(131, 20);
+            this.textBoxCELULAR.TabIndex = 5;
+            // 
+            // labelCELULAR
+            // 
+            this.labelCELULAR.AutoSize = true;
+            this.labelCELULAR.BackColor = System.Drawing.Color.Transparent;
+            this.labelCELULAR.ForeColor = System.Drawing.Color.White;
+            this.labelCELULAR.Location = new System.Drawing.Point(112, 83);
+            this.labelCELULAR.Name = "labelCELULAR";
+            this.labelCELULAR.Size = new System.Drawing.Size(39, 13);
+            this.labelCELULAR.TabIndex = 6;
+            this.labelCELULAR.Text = "Celular";
+            // 
+            // labelTELEFONE
+            // 
+            this.labelTELEFONE.AutoSize = true;
+            this.labelTELEFONE.BackColor = System.Drawing.Color.Transparent;
+            this.labelTELEFONE.ForeColor = System.Drawing.Color.White;
+            this.labelTELEFONE.Location = new System.Drawing.Point(9, 106);
+            this.labelTELEFONE.Name = "labelTELEFONE";
+            this.labelTELEFONE.Size = new System.Drawing.Size(49, 13);
+            this.labelTELEFONE.TabIndex = 7;
+            this.labelTELEFONE.Text = "Telefone";
+            // 
+            // textBoxTELEFONE
+            // 
+            this.textBoxTELEFONE.Location = new System.Drawing.Point(12, 125);
+            this.textBoxTELEFONE.Name = "textBoxTELEFONE";
+            this.textBoxTELEFONE.Size = new System.Drawing.Size(131, 20);
+            this.textBoxTELEFONE.TabIndex = 8;
+            // 
+            // buttonCADASTRAR
+            // 
+            this.buttonCADASTRAR.Location = new System.Drawing.Point(12, 311);
+            this.buttonCADASTRAR.Name = "buttonCADASTRAR";
+            this.buttonCADASTRAR.Size = new System.Drawing.Size(100, 23);
+            this.buttonCADASTRAR.TabIndex = 9;
+            this.buttonCADASTRAR.Text = "Cadastrar";
+            this.buttonCADASTRAR.UseVisualStyleBackColor = true;
+            this.buttonCADASTRAR.Click += new System.EventHandler(this.buttonCADASTRAR_Click);
+            // 
+            // buttonALTERAR
+            // 
+            this.buttonALTERAR.Location = new System.Drawing.Point(119, 311);
+            this.buttonALTERAR.Name = "buttonALTERAR";
+            this.buttonALTERAR.Size = new System.Drawing.Size(114, 23);
+            this.buttonALTERAR.TabIndex = 10;
+            this.buttonALTERAR.Text = "Alterar";
+            this.buttonALTERAR.UseVisualStyleBackColor = true;
+            this.buttonALTERAR.Click += new System.EventHandler(this.buttonALTERAR_Click);
+            // 
+            // buttonEXCLUIR
+            // 
+            this.buttonEXCLUIR.Location = new System.Drawing.Point(57, 340);
+            this.buttonEXCLUIR.Name = "buttonEXCLUIR";
+            this.buttonEXCLUIR.Size = new System.Drawing.Size(121, 23);
+            this.buttonEXCLUIR.TabIndex = 11;
+            this.buttonEXCLUIR.Text = "Excluir";
+            this.buttonEXCLUIR.UseVisualStyleBackColor = true;
+            this.buttonEXCLUIR.Click += new System.EventHandler(this.buttonEXCLUIR_Click);
+            // 
+            // groupBoxCONTATO
+            // 
+            this.groupBoxCONTATO.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxCONTATO.Controls.Add(this.radioButtonDESBLOQ);
+            this.groupBoxCONTATO.Controls.Add(this.radioButtonBLOQ);
+            this.groupBoxCONTATO.ForeColor = System.Drawing.Color.White;
+            this.groupBoxCONTATO.Location = new System.Drawing.Point(57, 224);
+            this.groupBoxCONTATO.Name = "groupBoxCONTATO";
+            this.groupBoxCONTATO.Size = new System.Drawing.Size(127, 81);
+            this.groupBoxCONTATO.TabIndex = 12;
+            this.groupBoxCONTATO.TabStop = false;
+            this.groupBoxCONTATO.Text = "Contato";
+            // 
+            // radioButtonDESBLOQ
+            // 
+            this.radioButtonDESBLOQ.AutoSize = true;
+            this.radioButtonDESBLOQ.ForeColor = System.Drawing.Color.White;
+            this.radioButtonDESBLOQ.Location = new System.Drawing.Point(6, 58);
+            this.radioButtonDESBLOQ.Name = "radioButtonDESBLOQ";
+            this.radioButtonDESBLOQ.Size = new System.Drawing.Size(94, 17);
+            this.radioButtonDESBLOQ.TabIndex = 3;
+            this.radioButtonDESBLOQ.TabStop = true;
+            this.radioButtonDESBLOQ.Text = "Desbloqueado";
+            this.radioButtonDESBLOQ.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewCONTATO
+            // 
+            this.dataGridViewCONTATO.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCONTATO.Location = new System.Drawing.Point(263, 12);
+            this.dataGridViewCONTATO.Name = "dataGridViewCONTATO";
+            this.dataGridViewCONTATO.Size = new System.Drawing.Size(389, 250);
+            this.dataGridViewCONTATO.TabIndex = 13;
+            this.dataGridViewCONTATO.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonPESQUISAR
+            // 
+            this.buttonPESQUISAR.Location = new System.Drawing.Point(263, 268);
+            this.buttonPESQUISAR.Name = "buttonPESQUISAR";
+            this.buttonPESQUISAR.Size = new System.Drawing.Size(389, 23);
+            this.buttonPESQUISAR.TabIndex = 14;
+            this.buttonPESQUISAR.Text = "Pesquisar";
+            this.buttonPESQUISAR.UseVisualStyleBackColor = true;
+            this.buttonPESQUISAR.Click += new System.EventHandler(this.buttonPESQUISAR_Click);
+            // 
+            // labelEMAIL
+            // 
+            this.labelEMAIL.AutoSize = true;
+            this.labelEMAIL.BackColor = System.Drawing.Color.Transparent;
+            this.labelEMAIL.ForeColor = System.Drawing.Color.White;
+            this.labelEMAIL.Location = new System.Drawing.Point(112, 148);
+            this.labelEMAIL.Name = "labelEMAIL";
+            this.labelEMAIL.Size = new System.Drawing.Size(35, 13);
+            this.labelEMAIL.TabIndex = 15;
+            this.labelEMAIL.Text = "E-mail";
+            // 
+            // textBoxEMAIL
+            // 
+            this.textBoxEMAIL.Location = new System.Drawing.Point(115, 164);
+            this.textBoxEMAIL.Name = "textBoxEMAIL";
+            this.textBoxEMAIL.Size = new System.Drawing.Size(131, 20);
+            this.textBoxEMAIL.TabIndex = 16;
+            // 
+            // labelLINKEDIN
+            // 
+            this.labelLINKEDIN.AutoSize = true;
+            this.labelLINKEDIN.BackColor = System.Drawing.Color.Transparent;
+            this.labelLINKEDIN.ForeColor = System.Drawing.Color.White;
+            this.labelLINKEDIN.Location = new System.Drawing.Point(9, 182);
+            this.labelLINKEDIN.Name = "labelLINKEDIN";
+            this.labelLINKEDIN.Size = new System.Drawing.Size(47, 13);
+            this.labelLINKEDIN.TabIndex = 17;
+            this.labelLINKEDIN.Text = "Linkedin";
+            // 
+            // textBoxLINKEDIN
+            // 
+            this.textBoxLINKEDIN.Location = new System.Drawing.Point(12, 198);
+            this.textBoxLINKEDIN.Name = "textBoxLINKEDIN";
+            this.textBoxLINKEDIN.Size = new System.Drawing.Size(131, 20);
+            this.textBoxLINKEDIN.TabIndex = 18;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(521, 353);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(131, 20);
+            this.textBoxID.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UC11_AGENDA.Properties.Resources.marcos_leonardo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(664, 447);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.textBoxLINKEDIN);
+            this.Controls.Add(this.labelLINKEDIN);
+            this.Controls.Add(this.textBoxEMAIL);
+            this.Controls.Add(this.labelEMAIL);
+            this.Controls.Add(this.buttonPESQUISAR);
+            this.Controls.Add(this.dataGridViewCONTATO);
+            this.Controls.Add(this.groupBoxCONTATO);
+            this.Controls.Add(this.buttonEXCLUIR);
+            this.Controls.Add(this.buttonALTERAR);
+            this.Controls.Add(this.buttonCADASTRAR);
+            this.Controls.Add(this.textBoxTELEFONE);
+            this.Controls.Add(this.labelTELEFONE);
+            this.Controls.Add(this.labelCELULAR);
+            this.Controls.Add(this.textBoxCELULAR);
+            this.Controls.Add(this.textBoxSOBRENOME);
+            this.Controls.Add(this.labelSOBRENOME);
+            this.Controls.Add(this.textBoxNOME);
+            this.Controls.Add(this.labelNOME);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxCONTATO.ResumeLayout(false);
+            this.groupBoxCONTATO.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCONTATO)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNOME;
+        private System.Windows.Forms.TextBox textBoxNOME;
+        private System.Windows.Forms.RadioButton radioButtonBLOQ;
+        private System.Windows.Forms.Label labelSOBRENOME;
+        private System.Windows.Forms.TextBox textBoxSOBRENOME;
+        private System.Windows.Forms.TextBox textBoxCELULAR;
+        private System.Windows.Forms.Label labelCELULAR;
+        private System.Windows.Forms.Label labelTELEFONE;
+        private System.Windows.Forms.TextBox textBoxTELEFONE;
+        private System.Windows.Forms.Button buttonCADASTRAR;
+        private System.Windows.Forms.Button buttonALTERAR;
+        private System.Windows.Forms.Button buttonEXCLUIR;
+        private System.Windows.Forms.GroupBox groupBoxCONTATO;
+        private System.Windows.Forms.RadioButton radioButtonDESBLOQ;
+        private System.Windows.Forms.DataGridView dataGridViewCONTATO;
+        private System.Windows.Forms.Button buttonPESQUISAR;
+        private System.Windows.Forms.Label labelEMAIL;
+        private System.Windows.Forms.TextBox textBoxEMAIL;
+        private System.Windows.Forms.Label labelLINKEDIN;
+        private System.Windows.Forms.TextBox textBoxLINKEDIN;
+        private System.Windows.Forms.TextBox textBoxID;
+    }
+}
+
---
+++ UC11_AGENDA/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace UC11_AGENDA
+{
+    public partial class Form1 : Form
+    {
+        string servidor;
+        MySqlConnection conexao;
+        MySqlCommand comando;
+
+        public Form1()
+        {
+            InitializeComponent();
+            servidor = "Server=localhost;Database=agenda_bd;Uid=root;Pwd=";
+            conexao = new MySqlConnection(servidor);
+            comando = conexao.CreateCommand();
+        }
+
+        private void buttonCADASTRAR_Click(object sender, EventArgs e)
+        {
+            bool duplicado = false;
+
+            try
+            {
+                conexao.Open();
+                comando.CommandText = " SELECT nome, sobrenome FROM tbl_contatos WHERE nome, sobrenome  = '" + textBoxNOME.Text + "'"+ textBoxSOBRENOME.Text +"'";
+                MySqlDataReader resultado = comando.ExecuteReader();
+
+                if (resultado.Read())
+                {
+                    duplicado = true;
+                    MessageBox.Show("Contato ja cadastrado!");
+                }
+            }
+            catch (Exception erro_mysql)
+            {
+                
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            if (duplicado == false)
+            {
+                try
+                {
+                    conexao.Open();
+                    comando.CommandText = "INSERT INTO tbl_contatos(nome, sobrenome) VALUES ('" + textBoxNOME.Text + "', '" + textBoxSOBRENOME.Text + "');";
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Contato cadastrado com sucesso!");
+                }
+                catch (Exception erro_mysql)
+                {
+                    MessageBox.Show(erro_mysql.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxNOME.Text = dataGridViewCONTATO.CurrentRow.Cells[2].Value.ToString();
+            textBoxSOBRENOME.Text = dataGridViewCONTATO.CurrentRow.Cells[3].Value.ToString();
+            textBoxCELULAR.Text = dataGridViewCONTATO.CurrentRow.Cells[5].Value.ToString();
+            
+        }
+
+        private void buttonPESQUISAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao.Open();
+                comando.CommandText = "SELECT * FROM tbl_contatos";
+
+                MySqlDataAdapter adaptadorcontatos = new MySqlDataAdapter(comando);
+
+                DataTable tabelacontatos = new DataTable();
+                adaptadorcontatos.Fill(tabelacontatos);
+
+                dataGridViewCONTATO.DataSource = tabelacontatos;
+                dataGridViewCONTATO.Columns["nome"].HeaderText = "Nome";
+                dataGridViewCONTATO.Columns["sobrenome"].HeaderText = "Sobrenome";
+                dataGridViewCONTATO.Columns["celular"].HeaderText = "Celular";
+                
+            }
+            catch (Exception erro_mysql)
+            {
+                MessageBox.Show(erro_mysql.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void buttonALTERAR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conexao.Open();
+                comando.CommandText = "UPDATE tbl_contatos SET nome = '" + textBoxNOME.Text + "', sobrenome = '" + textBoxSOBRENOME.Text + "', telefone = " + textBoxTELEFONE.Text + "', celular = " +textBoxCELULAR.Text + "', email = " +textBoxEMAIL.Text + "', linkedin = " +textBoxLINKEDIN.Text + "' WHERE id = " + textBoxID.Text + ";";
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Resgistro alterado com sucesso!!");
+
+            }
+            catch (Exception erro_mysql)
+            {
+                MessageBox.Show(erro_mysql.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        private void buttonEXCLUIR_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                conexao.Open();
+                comando.CommandText = "DELETE FROM tbl_contatos WHERE id = '" + textBoxID.Text + "';";
+                comando.ExecuteNonQuery();
+                MessageBox.Show("contato excluido com sucesso!");
+
+
+            }
+            catch (Exception erro)
+            {
+                
+                MessageBox.Show("Erro ao excluir contato. Tente Novamente");
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+    }
+}
